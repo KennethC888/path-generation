8e202b9561359ed14309c0804628eba389e7bb47
KChanChanChan <kenneth.chan@cooper.edu>
2025-02-28 15:35:21 -0500

    Parsed input and output flags

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 # Line 1 adds shebang to make it work like a shell script 
+import argparse
 
 def main():
-    pass
+    print("This is my first IGVC assingment regarding argparse.")
+    parser = argparse.ArgumentParser(description="This is an argparse")
+    parser.add_argument('-i', '--input')
+    parser.add_argument('-o', '--output')
+    args = parser.parse_args()
+    print(args)
 
 if __name__=="__main__":
     main()
