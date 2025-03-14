76b92739a358ffd25b303c26dde2ac3a662f656f
KChanChanChan <kenneth.chan@cooper.edu>
2025-03-14 16:44:27 -0400

    Added to .envrc file

---
+++ build/lib/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import argparse
+
+from pathlib import Path 
+
+def main():
+    parser = argparse.ArgumentParser(description="This program uses A* to create the optimal path for a car") 
+    parser.add_argument('-i', '--input', default = Path("input.yaml"), type = Path) 
+    parser.add_argument('-o', '--output', default = Path("output.yaml"), type = Path)
+    args = parser.parse_args()
+    print(args)
+
+if __name__=="__main__":
+    main()
